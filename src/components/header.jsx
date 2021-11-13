222b5643583acb0a0964be75fd984a0905b5e9ff
Chiranjeeb Nayak <chiranjeebnayak.37@gmail.com>
2021-11-13 21:08:23 +0530

    header push

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -10,12 +10,12 @@ function Header() {
         <div className="row">
         
           <div className="col-4 col-sm-4 d-flex align-items-center">
-            <label><HiTrendingUp className={styles.trend} /></label>
+            <label><HiTrendingUp className={styles.trend} /></label>&nbsp;
             <label>SPORTSAO</label>
           </div>
           <div className="col-8 col-sm-6  col-md-6">
             <div className="input-group">
-              <input className="form-control"></input>
+              <input className="form-control" placeholder="Search Player"></input>
               <button className="btn btn-primary ">
                 <FiSearch />
               </button>
